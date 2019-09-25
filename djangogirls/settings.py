3fdfaac18004688a099881434d3b21a284f61698
Angus Allman <allmanaj@users.noreply.github.com>
2019-09-25 18:17:30 +0100

    Update settings.py

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -186,12 +186,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 DJANGO_GULP_REV_PATH = os.path.join(BASE_DIR, 'static/rev-manifest.json')
 LOGIN_URL = 'admin:login'
 
-if 'TRAVIS' in os.environ:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/source')]
-elif DEBUG:
+#if 'TRAVIS' in os.environ:
+    #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/source')]
+#elif DEBUG:
     #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/local')]
-else:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/build')]
+#else:
+#    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/build')]
 
 
 STATICFILES_FINDERS = (
