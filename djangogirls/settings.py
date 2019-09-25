03f2f4c6fc2a1348ebc0d19eb36cea61b2615066
Angus Allman <allmanaj@users.noreply.github.com>
2019-09-25 18:16:31 +0100

    Update settings.py

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -189,7 +189,7 @@ LOGIN_URL = 'admin:login'
 if 'TRAVIS' in os.environ:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/source')]
 elif DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/local')]
+    #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/local')]
 else:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/build')]
 
